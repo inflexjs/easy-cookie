20d96f9b15e45d232fc92bffddb472add9474ac5
inflexjs <helidanger@yandex.ru>
2021-10-28 19:14:14 +0300

    anim bottom modal/accept btn

--- js/cookiepp.js
+++ js/cookiepp.js
@@ -1,11 +1,21 @@
-let modalCookieBottom = document.querySelector(".modal__cookie-bottom")
+let modalCookieBottom = document.querySelector('.modal__cookie-bottom');
+let cookieAcceptBtn = document.querySelector('.cookie__btn');
 
+let showCookie = function () {
+	modalCookieBottom.classList.add('modal__cookie-bottom-active');
+};
 
-let showCookie = function() {
-	modalCookieBottom.classList.add("modal__cookie-bottom-active")
-}
+window.onload = function () {
+	setTimeout(showCookie, 1000);
+};
 
+cookieAcceptBtn.addEventListener('click', () => {
+	modalCookieBottom.classList.add('modal__cookie-bottom-accepted');
+	modalCookieBottom.classList.remove('modal__cookie-bottom-active');
 
-window.onload = function() {
-	setTimeout(showCookie, 3000)
-}
+	function removeCookieBanner() {
+		modalCookieBottom.remove();
+	}
+
+	setTimeout(removeCookieBanner, 1500);
+});
