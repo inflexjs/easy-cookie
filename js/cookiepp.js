1da773e5fd391582199d65445b918384b59c0fe0
inflexjs <helidanger@yandex.ru>
2021-10-29 15:02:13 +0300

    left/right/js logic

--- js/cookiepp.js
+++ js/cookiepp.js
@@ -1,30 +1,17 @@
 let modalCookie = document.querySelector('.modal');
 let cookieAcceptBtn = document.querySelector('.cookie__btn');
-let leftModal = document.querySelector('.left-modal');
-let topModal = document.querySelector('.top-modal');
-let bottomModal = document.querySelector('.bottom-modal');
-let rigthModal = document.querySelector('.right-modal');
 let buttons = document.querySelectorAll('.button');
 
 let isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)
+
 // let showCookie = function () {
 // 	modalCookie.classList.add('cookie-bottom--active');
+// 	modalCookie.classList.add('cookie-bottom');
 // };
 
 // window.onload = function () {
 // 	setTimeout(showCookie, 1000);
 // };
-
-// cookieAcceptBtn.addEventListener('click', () => {
-// 	modalCookie.classList.add('cookie-bottom--accepted');
-// 	modalCookie.classList.remove('ccookie-bottom--active');
-
-// 	// function removeCookieBanner() {
-// 	// 	modalCookie.remove();
-// 	// }
-
-// 	// setTimeout(removeCookieBanner, 1500);
-// });
 
 for (let i = 0; i < buttons.length; i++) {
 	let active = [
@@ -50,6 +37,7 @@ for (let i = 0; i < buttons.length; i++) {
 			modalCookie.classList.remove(active[a]);
 			modalCookie.classList.remove(position[a]);
 			modalCookie.classList.remove(accepted[a]);
+			modalCookie.classList.remove('hidden');
 		}
 
 		buttons[i].classList.toggle('button--current');
@@ -67,33 +55,22 @@ for (let i = 0; i < buttons.length; i++) {
 			modalCookie.classList.add(active[i]);
 			modalCookie.classList.add(position[i]);
 
-			function removeActive() {
-				modalCookie.classList.remove(accepted[i]);
-				modalCookie.classList.remove(position[i]);
-				modalCookie.classList.remove(active[i]);
-			}
+			// function removeActive() {
+			// 	modalCookie.classList.add('hidden');
+			// 	// modalCookie.classList.remove(accepted[i]);
+			// 	// modalCookie.classList.remove(position[i]);
+			// 	// modalCookie.classList.remove(active[i]);
+			// }
 
-			if (window.innerHeight <= 1000) {
-				setTimeout(removeActive, 400);
-				console.log('mobile')
-			} else {
-				setTimeout(removeActive, 300);
-				console.log('pc')
-			}
+			// if (window.innerHeight <= 1000) {
+			// 	setTimeout(removeActive, 400);
+			// 	console.log('mobile')
+			// } else {
+			// 	setTimeout(removeActive, 300);
+			// 	console.log('pc')
+			// }
 
 			buttons.forEach((i) => i.classList.remove('button--current'));
 		});
 	});
 }
-
-// for (let button of buttons) {
-// 	button.addEventListener('click', function () {
-// 		buttons.forEach((i) => i.classList.remove('button--current'));
-
-// 		button.classList.toggle('button--current');
-// 		for
-// 		if (buttons[button] == 'bottom-modal') {
-// 			modalCookie.classList.add('cookie-bottom--active');
-// 		}
-// 	});
-// }
