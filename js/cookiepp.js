65b5edc6bc6326a141b97829a49463d13610a912
inflexjs <helidanger@yandex.ru>
2021-10-31 01:29:06 +0300

    cookie save/delete

--- js/cookiepp.js
+++ js/cookiepp.js
@@ -1,7 +1,8 @@
 let cookieModal = document.querySelector('.modal');
 let cookieAcceptBtn = document.querySelector('.modal__accept-btn');
 let cookieCloseBtn = document.querySelector('.modal__close-btn');
-let cookieStatus = document.querySelector('.cookie-status');
+let cookieCleareBtn = document.querySelector('.clear-cookie');
+let cookieStatusText = document.querySelector('.cookie-status');
 let buttons = document.querySelectorAll('.button');
 let leftBtn = document.querySelector('.left-modal');
 let rightBtn = document.querySelector('.right-modal');
@@ -29,14 +30,62 @@ if (isMobile && window.innerWidth <= 620) {
 	mainWindow.appendChild(disabledText);
 }
 
+if (window.innerWidth <= 400) {
+	cookieStatusText.textContent = '<br> Привет';
+}
+
+function setCookie(name, value, days) {
+	var expires = '';
+	if (days) {
+		var date = new Date();
+		date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+		expires = '; expires=' + date.toUTCString();
+	}
+	document.cookie = name + '=' + (value || '') + expires + '; path=/';
+}
+
+function getCookie(name) {
+	var nameEQ = name + '=';
+	var ca = document.cookie.split(';');
+	for (var i = 0; i < ca.length; i++) {
+		var c = ca[i];
+		while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+	}
+	return null;
+}
+
+function eraseCookie(name) {
+	document.cookie = name + '=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
+
+function deleteCookie(name) {
+	setCookie(name, '', {
+		'max-age': -1,
+	});
+}
+
 // let showCookie = function () {
 // 	cookieModal.classList.add('cookie-bottom--active');
 // 	cookieModal.classList.add('cookie-bottom');
 // };
 
-// window.onload = function () {
-// 	setTimeout(showCookie, 1000);
-// };
+let cookieStatus = getCookie('easycookie');
+
+if (cookieStatus) {
+	cookieStatusText.textContent = 'accepted';
+	cookieStatusText.classList.add('accepted');
+} else {
+	cookieStatusText.classList.add('undefined');
+	cookieStatusText.textContent = 'undefined';
+}
+
+cookieCleareBtn.addEventListener('click', () => {
+	deleteCookie('easycookie');
+	cookieStatusText.classList.add('undefined');
+	cookieStatusText.textContent = 'undefined';
+	window.location.reload();
+});
 
 for (let i = 0; i < buttons.length; i++) {
 	let position = ['cookie-left', 'cookie-top', 'cookie-bottom', 'cookie-right'];
@@ -58,10 +107,10 @@ for (let i = 0; i < buttons.length; i++) {
 	buttons[i].addEventListener('click', function () {
 		buttons.forEach((i) => i.classList.remove('button--current'));
 
-		cookieStatus.classList.remove('skipped');
-		cookieStatus.classList.remove('accepted');
-		cookieStatus.classList.add('undefined');
-		cookieStatus.textContent = 'undefined';
+		cookieStatusText.classList.remove('skipped');
+		cookieStatusText.classList.remove('accepted');
+		cookieStatusText.classList.add('undefined');
+		cookieStatusText.textContent = 'undefined';
 
 		function hideModal() {
 			for (let d = 0; d < active.length; d++) {
@@ -88,22 +137,19 @@ for (let i = 0; i < buttons.length; i++) {
 		cookieModal.classList.add(active[i]);
 		cookieModal.classList.add(position[i]);
 
-		if (isMobile) {
-			cookieModal.classList.add('modal-mobile');
-		}
-
 		cookieAcceptBtn.addEventListener('click', () => {
 			hideModal();
-			cookieStatus.textContent = 'accepted';
-			cookieStatus.classList.remove('undefined');
-			cookieStatus.classList.add('accepted');
+			setCookie('easycookie', 'testeasycookie', 3);
+			cookieStatusText.textContent = 'accepted';
+			cookieStatusText.classList.remove('undefined');
+			cookieStatusText.classList.add('accepted');
 		});
 
 		cookieCloseBtn.addEventListener('click', () => {
 			hideModal();
-			cookieStatus.textContent = 'skipped';
-			cookieStatus.classList.remove('undefined');
-			cookieStatus.classList.add('skipped');
+			cookieStatusText.textContent = 'skipped';
+			cookieStatusText.classList.remove('undefined');
+			cookieStatusText.classList.add('skipped');
 		});
 	});
 }
